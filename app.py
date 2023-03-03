ed1a30fb1889ad785c455527693f1179c535a51d
Lucy Newman <lucy.newman@arcadia.com>
2023-03-03 17:42:02 -0500

    writing rendered html to a file

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     image_names = os.listdir('static')
-    return render_template('images.html', image_names=image_names)
+    image_urls = ['static/' + name for name in image_names]
+    rendered_template = render_template('images.html', image_urls=image_urls)
+    with open('flask_images_demo.html', 'w') as f:
+        f.write(rendered_template)
+    return rendered_template
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=81)
+    print("hi")
